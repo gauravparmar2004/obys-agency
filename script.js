7f15b9ebd43bc261cfb971fba2e32f1cb610c04f
Gaurav <gaurav.1222parmar@gmail.com>
2024-06-10 22:57:27 +0530

    Page1 Completed

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
-var tl = gsap.timeline();
+function loadingAnimation() {
+  var tl = gsap.timeline();
 
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.25,
-  duration: 0.5,
-  delay: 0.1,
-});
-tl.from("#line1-part1 h5", {
-  opacity: 0,
-  onStart: function () {
-    var h5Timer = document.querySelector("#line1-part1 h5");
-    var grow = 0;
-    setInterval(function () {
-      if (grow < 100) {
-        h5Timer.innerHTML = grow++;
-      } else {
-        grow = 100;
-        h5Timer.innerHTML = grow;
-      }
-    }, 20);
-  },
-});
-tl.to(".line h2", {
-  opacity: 1,
-  animationName: "anime",
-});
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.5,
-  delay: 3.2,
-});
-tl.from("#page1", {
-  delay: 0.5,
-  y: 1600,
-  opacity: 0,
-  duration: 0.5,
-  ease: Power4,
-});
-tl.to("#loader", {
-  display: "none",
-});
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.5,
+    delay: 0.1,
+  });
+  tl.from("#line1-part1 h5", {
+    opacity: 0,
+    onStart: function () {
+      var h5Timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
+      setInterval(function () {
+        if (grow < 100) {
+          h5Timer.innerHTML = grow++;
+        } else {
+          grow = 100;
+          h5Timer.innerHTML = grow;
+        }
+      }, 20);
+    },
+  });
+  tl.to(".line h2", {
+    opacity: 1,
+    animationName: "anime",
+  });
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.5,
+    delay: 3.2,
+  });
+  tl.from("#page1", {
+    delay: 0.5,
+    y: 1600,
+    opacity: 0,
+    duration: 0.5,
+    ease: Power4,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+}
+loadingAnimation();
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+
+  Shery.makeMagnet("#nav-part2 h4", {});
+}
+cursorAnimation();
