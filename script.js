3ea2e586aed9b2f012ab52ef655e6f69b4f0e325
Gaurav <gaurav.1222parmar@gmail.com>
2024-07-05 10:14:33 +0530

    add flag animation

--- script.js
+++ script.js
@@ -185,3 +185,21 @@ locomotiveAnimation();
 loadingAnimation();
 cursorAnimation();
 sheryAnimation();
+
+document.addEventListener("mousemove", function (dets) {
+  gsap.to("#flag", {
+    x: dets.x,
+    y: dets.y,
+  });
+});
+document.querySelector("#hero-3").addEventListener("mouseenter",function(){
+  gsap.to("#flag",{
+    opacity:1
+  })
+})
+
+document.querySelector("#hero-3").addEventListener("mouseleave",function(){
+  gsap.to("#flag",{
+    opacity:0
+  })
+})
